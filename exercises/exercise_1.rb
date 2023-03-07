6877f911eb39b22d1f6c0a2a64e18b45c972ff92
mikedaltonmtl <michael.j.dalton@gmail.com>
2023-03-07 21:46:13 +0000

    initial commit

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,6 +1,12 @@
 require_relative '../setup'
 
+=begin
+Exercise 1: Employee passwords
+Scenario: We want to be able to give employees a password (string) that is
+auto-generated when their record is created in the database.
+=end
+
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+
